fe381c7fd4542821b3b8c5f9ade223dd1bedab67
Fabian Schlatter <fschlatt@protonmail.com>
2025-03-05 14:55:28 +0100

    chore: Add Font Awesome library for icons - true copilot ma pravdu jdu kms

--- app/Views/layout/cssjs.php
+++ app/Views/layout/cssjs.php
@@ -1,3 +1,4 @@
 <script src=<?=base_url("node_modules/bootstrap/dist/js/bootstrap.min.js")?>></script>
 <link rel="stylesheet" href="<?=base_url("node_modules/bootstrap/dist/css/bootstrap.min.css")?>">
+<script src="<?=base_url("node_modules/fontawesome-free/js/all.min.js")?>"></script>
 <link rel="stylesheet" href="<?=base_url("node_modules/fontawesome-free/css/all.min.css")?>">
